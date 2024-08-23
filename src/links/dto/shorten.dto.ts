7eaa484e766645c0da5a0ac2fce1546c859cb49b
mykhailokrachun <mykhailokrachun@gmail.com>
2024-08-23 15:14:09 +0300

    added swagger docs

--- src/links/dto/shorten.dto.ts
+++ src/links/dto/shorten.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUrl } from 'class-validator';
 
 export class ShortenDto {
+  @ApiProperty({
+    description: 'The URL to be shortened',
+    example: 'https://example.com',
+  })
   @IsUrl({}, { message: 'The provided value must be a valid URL' })
   url: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -8,6 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Shortener API')
+    .setVersion('1.0')
+    .addTag('shortener')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
